06bbbd0d0c34dfb1ea9aa1c93a6cd35e807b9427
farnaziifz <ffarzipour92@gmail.com>
2019-01-25 21:51:44 +0330

    first commit

---
+++ src/components/video_list_items.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const VideoListItem = ({video, onVideoSelect}) => {
+    const imageURL = video.snippet.thumbnails.default.url
+    const mediaHeading = video.snippet.title
+    console.log(video)
+    return (
+        <li className="list-group-item" onClick={() => {onVideoSelect(video)}}>
+            <div className="video-list media">
+                <div className="media-left" >
+                    <img className="media-object" src={imageURL}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{mediaHeading}</div>
+                </div>
+            </div>
+        </li>
+    )
+
+}
+
+export default VideoListItem
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import _ from 'lodash'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_details'
+
+const API_KEY = 'AIzaSyCv0f-oYdTmKfv3IZWvznYfj4KaWyf_Sl0'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {   
+        videos: [],
+        selectedVideo: null
+    }
+    this.videoSearch('salam')
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+  })
+  }
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+    return(
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect = {(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+        
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.querySelector('#root'))
+
